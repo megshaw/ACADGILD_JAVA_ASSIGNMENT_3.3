77e7ca3885c441d41cee5a15d46ff072ea5b5372
meghana <meghana@Namsystech-Dev-01>
2017-03-24 17:40:44 +0530

    Assignment 3.3

---
+++ JavaAssignment 3.3/src/Assignment3.java
@@ -0,0 +1,20 @@
+
+
+
+public class Assignment3 {
+    int x;int y;
+    String name;
+    public static void main(String args[])
+    {
+        Assignment3 pnt =new Assignment3();
+        System.out.println ("pnt is " + pnt.name + " " + pnt.x + " " + pnt.y);
+        
+        
+    }
+    
+}
+
+
+
+
+/*Answer is null*/
